279b6e01d0735b1444ccb81f9389ca5262300763
romulofull <romulo.castro@gmail.com>
2024-07-31 22:00:36 -0500

    Funciones en Javascript

---
+++ js/function.js
@@ -0,0 +1,19 @@
+function saludar() {
+console.log ("Hola, Estoy Programando :");
+}
+
+saludar();
+saludar();
+saludar();
+saludar();
+
+
+function sumar (num1,num2) {
+let resultado;
+resultado = num1 + num2;
+return resultado;
+}
+
+let suma = sumar(5,2);
+console.log ("El resultado es "+ suma);
+
